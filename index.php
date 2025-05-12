ea52ff614bd79bceaf69b16a3cf278e81192a585
Adryan Aulia Al Tazani <adryanaltazani08@gmail.com>
2025-05-12 11:11:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestiae ratione reprehenderit sint laborum doloribus facere incidunt accusantium iure enim reiciendis aliquam repellendus, aspernatur molestias odit facilis, dolor earum odio.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
